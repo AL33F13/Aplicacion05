f6e6581b8a2a57d4a0f7f5d9069fda1125129e47
AL33F13 <joss.marvell@gmail.com>
2015-04-21 14:34:34 -0500

    uno
    nuevos archivos

---
+++ js/acciones.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+$(document).ready(function(e) {
+//document.addEventListener("deviceready",function(){
+  
+   $('#btndatos').on('click',function(){
+	  alert('Holaaas');
+	  $('body').pagecontainer("change", "#datos",{transition:"flip"});
+  }); //Click btndatos 
+  
+  $('#btnresultado').on('click',function(){
+	  
+	  $('body').pagecontainer("change", "#resultado", {transition:"flip"}); 
+  
+  var imc;
+  var peso= $('#txtpeso').val();
+  var talla= $('#txttalla').val();
+ 
+  imc= (peso/(talla*talla));
+  $('#imc').text('Nombre ' + $('txtnombre').val() + ' imc= '+imc)
+  });//Click btnresultado
+//}); 
+});
